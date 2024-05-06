0f1d748cd37f2fd291dfddab9f96e893929a546a
Sohrab Behdani <behdanisohrab@gmail.com>
2024-05-07 00:12:16 +0330

    removed grub from template

--- iso/profiledef.sh
+++ iso/profiledef.sh
@@ -3,18 +3,18 @@
 
 iso_name="Parchlinux"
 iso_label="PARCH_$(date +%Y%m)"
-iso_publisher="Parch Linux <https://parchlinux.ir>"
+iso_publisher="Parch Linux <https://parchlinux.com>"
 iso_application="Parch Linux Live/Rescue CD"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
-	   'uefi-ia32.grub.esp' 'uefi-x64.systemd-boot.esp'
-	   'uefi-ia32.grub.eltorito' 'uefi-x64.systemd-boot.eltorito')
+	    'uefi-x64.systemd-boot.esp'
+	    'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
-airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86')
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/root"]="0:0:750"
